ed0d15e4728ac7e539cbd9faea8ff4067aca0064
Dawid Matuszak <dawid_matuszak@outlook.com>
2017-08-02 18:02:16 +0200

    Generate and persist samples to database

--- src/main/java/com/matuszak/projects/Application.java
+++ src/main/java/com/matuszak/projects/Application.java
@@ -1,66 +1,62 @@
 package com.matuszak.projects;
 
-import com.matuszak.projects.project.ProjectRepository;
+import com.matuszak.projects.authorization.Role;
+import com.matuszak.projects.user.User;
 import com.matuszak.projects.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-/**
- * Created by dawid on 16.02.17.
- */
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableWebMvc
-public class Application {
+@EnableAutoConfiguration
+@ComponentScan
+public class Application{
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    UserRepository userRepository;
+    public Application(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
-    @Autowired
-    ProjectRepository projectRepository;
+    @PostConstruct
+    public void setUpDatabase(){
+
+        User build = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("admin"))
+                .enabled(true)
+                .email("admin@admin.com")
+                .userRole(Role.ADMIN)
+                .firstName("dawid")
+                .lastName("matuszak")
+                .build();
+
+        User user = User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("user"))
+                .enabled(true)
+                .email("userowo@mobile.com")
+                .userRole(Role.USER)
+                .firstName("kasia")
+                .lastName("nowak")
+                .build();
+
+        this.userRepository.save(Arrays.asList(user,build));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-//    @PostConstruct
-//    public void setUpDatabase(){
-//
-//        User admin = User.builder()
-//                .username("admin")
-//                .password("admin")
-//                .email("admin@admin.com")
-//                .enabled(true)
-//                .userRole(Role.USER)
-//                .experience(Arrays.asList(ProgrammingLanguages.JAVA, ProgrammingLanguages.PHP))
-//                .firstName("Admin")
-//                .lastName("Admin")
-//                .build();
-//
-//        User user = User.builder()
-//                .username("user")
-//                .password("user")
-//                .email("user@gmail.com")
-//                .enabled(true)
-//                .userRole(Role.USER)
-//                .experience(Arrays.asList(ProgrammingLanguages.JAVA, ProgrammingLanguages.PHP))
-//                .firstName("Mateusz")
-//                .lastName("Stanek")
-//                .build();
-//
-//        Project project = Project.builder()
-//                .createdDate(new Date(new GregorianCalendar().getTime().getTime()))
-//                .name("FirstProject")
-//                .uuid("12312-23123-421123-213123")
-//                .description("FirstDescription")
-//                .owner(user)
-//                .participants(Arrays.asList(user))
-//                .technologies(Arrays.asList(ProgrammingLanguages.PHP))
-//                .build();
-//
-//        this.userRepository.save(Arrays.asList(user,admin));
-//        this.projectRepository.save(project);
-//    }
 }
