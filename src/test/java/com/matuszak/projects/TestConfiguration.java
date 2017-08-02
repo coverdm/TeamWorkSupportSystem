fcf418880ec5c7d65d1321dbdc38a9f7215c02cd
Dawid Matuszak <dawid_matuszak@outlook.com>
2017-08-02 18:07:00 +0200

    Added support for tests to work with spring security

---
+++ src/test/java/com/matuszak/projects/TestConfiguration.java
@@ -0,0 +1,9 @@
+package com.matuszak.projects;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@PropertySource("classpath:test.properties")
+@Configuration
+public class TestConfiguration {
+}
