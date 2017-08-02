3ef195922d4ed4402566d28f8f7fba4048bc261e
Dawid Matuszak <dawid_matuszak@outlook.com>
2017-08-02 18:03:41 +0200

    Removed author's header

--- src/main/java/com/matuszak/projects/user/UserRepository.java
+++ src/main/java/com/matuszak/projects/user/UserRepository.java
@@ -1,16 +1,12 @@
 package com.matuszak.projects.user;
 
-import com.matuszak.projects.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-/**
- * Created by dawid on 21.03.17.
- */
 public interface UserRepository extends JpaRepository<User, Long>{
-    Optional<User> findUserByUsername(String username);
 
+    Optional<User> findUserByUsername(String username);
     List<User> getUsersByUsernameIn(List<String> user);
 }
