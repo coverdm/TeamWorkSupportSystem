ca9c057bab18e39594ee92be804aac5f301bd81a
Dawid Matuszak <dawid_matuszak@outlook.com>
2017-08-03 19:24:37 +0200

    Added http response for exception

--- src/main/java/com/matuszak/projects/project/ProjectNotFoundException.java
+++ src/main/java/com/matuszak/projects/project/ProjectNotFoundException.java
@@ -1,10 +1,13 @@
 package com.matuszak.projects.project;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No a such project")
 public class ProjectNotFoundException extends RuntimeException{
 
     public ProjectNotFoundException(String s) {
         super(s);
     }
-
 
 }
