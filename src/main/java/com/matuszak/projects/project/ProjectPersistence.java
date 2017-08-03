ffed460962e781faf40bdff530c38c4469c85f85
Dawid Matuszak <dawid_matuszak@outlook.com>
2017-08-03 19:23:49 +0200

    Separated some method into another classes. Refactored

--- src/main/java/com/matuszak/projects/project/ProjectPersistence.java
+++ src/main/java/com/matuszak/projects/project/ProjectPersistence.java
@@ -1,9 +1,6 @@
 package com.matuszak.projects.project;
 
-import com.matuszak.projects.user.User;
-
 import java.util.List;
-import java.util.Optional;
 
 public interface ProjectPersistence {
 
@@ -11,9 +8,5 @@ public interface ProjectPersistence {
     List<Project> getProjectsByOwnerUsername(String username);
 
     Project saveProject(Project project);
-    Project addParticipants(String uuid, List<User> users);
     Project getProjectByUUID(String uuid);
-
-    Project finishProject(String uuid);
-    Project changeStatus(String uuid, ProjectStatus status);
 }
--- src/main/java/com/matuszak/projects/project/ProjectPersistenceImpl.java
+++ src/main/java/com/matuszak/projects/project/ProjectPersistenceImpl.java
@@ -1,14 +1,9 @@
 package com.matuszak.projects.project;
 
-import com.matuszak.projects.user.UserPersistence;
-import com.matuszak.projects.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
-import java.util.UUID;
 import java.util.logging.Logger;
 
 @Service
@@ -16,12 +11,10 @@ public class ProjectPersistenceImpl implements ProjectPersistence {
 
     private final Logger logger = Logger.getLogger(getClass().getName());
     private final ProjectRepository projectRepository;
-    private final UserPersistence userPersistence;
 
     @Autowired
-    public ProjectPersistenceImpl(ProjectRepository projectRepository, UserPersistence userPersistence) {
+    public ProjectPersistenceImpl(ProjectRepository projectRepository) {
         this.projectRepository = projectRepository;
-        this.userPersistence = userPersistence;
     }
 
     @Override
@@ -31,19 +24,6 @@ public class ProjectPersistenceImpl implements ProjectPersistence {
 
     @Override
     public Project saveProject(Project project) {
-
-        project.setUuid(UUID.randomUUID().toString());
-
-        String loggedUsername = getLoggedUsername();
-
-        User owner = userPersistence.getUserByUsername(loggedUsername);
-
-        project.setOwner(owner);
-
-        project.setCreatedDate(LocalDate.now());
-
-        logger.info("Created project: " + project.toString());
-
         return projectRepository.save(project);
     }
 
@@ -53,55 +33,8 @@ public class ProjectPersistenceImpl implements ProjectPersistence {
     }
 
     @Override
-    public Project addParticipants(String uuid, List<User> users) {
-
-        Project projectByUUID = null;
-
-        try{
-
-            projectByUUID = this.getProjectByUUID(uuid);
-            List<User> participants = projectByUUID.getParticipants();
-            users.forEach(participants::add);
-
-        }catch(ProjectNotFoundException e){
-            e.printStackTrace();
-        }
-
-        logger.info("Added participants: " + users.toString() + " to project: " + uuid);
-
-        return this.projectRepository.save(projectByUUID);
-    }
-
-    @Override
-    public Project getProjectByUUID(String uuid) throws ProjectNotFoundException{
+    public Project getProjectByUUID(String uuid) throws ProjectNotFoundException {
         return this.projectRepository.getProjectByUuid(uuid)
                 .orElseThrow(() -> new ProjectNotFoundException("Project not found"));
-    }
-
-    private String getLoggedUsername() {
-        return SecurityContextHolder.getContext().getAuthentication().getName();
-    }
-
-    @Override
-    public Project changeStatus(String uuid, ProjectStatus status) {
-
-        Project projectByUUID = null;
-
-        try{
-            projectByUUID = getProjectByUUID(uuid);
-            projectByUUID.setStatus(status);
-        }catch (ProjectNotFoundException e){
-            e.printStackTrace();
-        }
-
-        return this.projectRepository.save(projectByUUID);
-    }
-
-    @Override
-    public Project finishProject(String uuid) {
-
-        Project projectByUUID = getProjectByUUID(uuid);
-        projectByUUID.setStatus(ProjectStatus.FINISHED);
-        return projectByUUID;
     }
 }
