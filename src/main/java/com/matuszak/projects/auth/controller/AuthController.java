a61e6bdd7ef3bf8fcf07a7de04906f7d7d6d6f7d
Dawid Matuszak <dawid_matuszak@outlook.com>
2017-10-14 23:27:19 +0200

    Changed route and migrated

--- src/main/java/com/matuszak/projects/auth/controller/AuthController.java
+++ src/main/java/com/matuszak/projects/auth/controller/AuthController.java
@@ -1,39 +1,60 @@
 package com.matuszak.projects.auth.controller;
 
+import com.matuszak.projects.auth.domain.LoginModel;
+import com.matuszak.projects.auth.domain.RegisterModel;
 import com.matuszak.projects.auth.service.AuthenticationService;
+import com.matuszak.projects.auth.service.RegistrationProcess;
+import com.matuszak.projects.auth.exceptions.EmailAlreadyExistsException;
+import com.matuszak.projects.auth.exceptions.PasswordNotMatchedException;
+import com.matuszak.projects.user.dto.UserDTO;
 import com.matuszak.projects.user.entity.User;
-import com.matuszak.projects.user.service.UserPersistence;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import javax.naming.AuthenticationException;
+import javax.validation.Valid;
 import java.util.Map;
-import java.util.logging.Logger;
 
-@Deprecated
-@RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/auth")
+@RestController
 @Log
+@RequestMapping("/api/auth")
 public class AuthController {
 
     private final AuthenticationService authenticationService;
-    private final UserPersistence userPersistence;
+    private final RegistrationProcess registrationProcess;
+    private final ModelMapper modelMapper;
 
-    @PostMapping(value = "/login")
-    public ResponseEntity<Map<String, Object>> authenticate(@RequestBody User user) throws IOException {
-        log.info("Username: " + user.getUsername());
-        return new ResponseEntity<>(authenticationService.authenticate(user), HttpStatus.ACCEPTED);
+    @RequestMapping("/login")
+    public ResponseEntity<Map<String,Object>> login(@RequestBody LoginModel loginModel){
+
+        try {
+
+            Map<String, Object> authenticate = authenticationService.authenticate(loginModel);
+            return new ResponseEntity<>(authenticate, HttpStatus.OK);
+
+        } catch (AuthenticationException e) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
     }
 
-    @PostMapping(value = "/register")
-    public ResponseEntity<?> register(@RequestBody User user){
-        userPersistence.saveUser(user);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+    @RequestMapping("/register")
+    public ResponseEntity register(@Valid @RequestBody RegisterModel registerModel){
+
+        try{
+            User register = registrationProcess.register(registerModel);
+
+            UserDTO userDTO = modelMapper.map(register, UserDTO.class);
+
+            return new ResponseEntity(userDTO, HttpStatus.OK);
+        }catch (PasswordNotMatchedException | EmailAlreadyExistsException  e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
