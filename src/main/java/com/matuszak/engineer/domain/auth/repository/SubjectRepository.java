388426ad83911bdebb3de456e7a8717c51680ad4
Dawid Matuszak <dawid_matuszak@outlook.com>
2017-11-16 23:13:33 +0100

    Added method that returns subject by userId
    Signed-off-by: Dawid Matuszak <dawid_matuszak@outlook.com>

--- src/main/java/com/matuszak/engineer/domain/auth/repository/SubjectRepository.java
+++ src/main/java/com/matuszak/engineer/domain/auth/repository/SubjectRepository.java
@@ -1,11 +1,13 @@
 package com.matuszak.engineer.domain.auth.repository;
 
 import com.matuszak.engineer.domain.auth.model.entity.Subject;
+import com.matuszak.engineer.infrastructure.entity.UserId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface SubjectRepository extends JpaRepository<Subject, Long> {
+public interface SubjectRepository extends JpaRepository<Subject, UserId> {
     Optional<Subject> getSubjectByEmail(String email);
     Optional<Subject> getSubjectByUsername(String username);
+    Optional<Subject> getSubjectByUserId(UserId userId);
 }
