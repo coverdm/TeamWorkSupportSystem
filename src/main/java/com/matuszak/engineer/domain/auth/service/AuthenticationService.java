0a979c448463e3aa3535db95a952ff9e3753772d
Dawid Matuszak <dawid_matuszak@outlook.com>
2017-11-16 23:14:08 +0100

    Added method that checks does user exists
    Signed-off-by: Dawid Matuszak <dawid_matuszak@outlook.com>

--- src/main/java/com/matuszak/engineer/domain/auth/service/AuthenticationService.java
+++ src/main/java/com/matuszak/engineer/domain/auth/service/AuthenticationService.java
@@ -6,26 +6,35 @@ import com.matuszak.engineer.domain.auth.exceptions.PasswordNotMatchedException;
 import com.matuszak.engineer.domain.auth.model.dto.LoginModel;
 import com.matuszak.engineer.domain.auth.model.dto.RegisterModel;
 import com.matuszak.engineer.domain.auth.model.entity.Token;
+import com.matuszak.engineer.domain.auth.repository.SubjectRepository;
+import com.matuszak.engineer.infrastructure.entity.UserId;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Service;
 
 import javax.security.auth.login.LoginException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 @Log
 public class AuthenticationService {
 
+    private final SubjectRepository subjectRepository;
     private final LoginService loginService;
     private final RegistrationService registrationService;
 
-    public Token login(final LoginModel loginModel) throws LoginException {
+    public Map<String, Object> login(final LoginModel loginModel) throws LoginException {
         return loginService.login(loginModel);
     }
 
     public void register(final RegisterModel registerModel) throws PasswordNotMatchedException, EmailAlreadyExistsException {
         registrationService.register(registerModel);
+    }
+
+    public Boolean isUserExists(UserId userId){
+        return subjectRepository.getSubjectByUserId(userId).isPresent();
     }
 
 }
